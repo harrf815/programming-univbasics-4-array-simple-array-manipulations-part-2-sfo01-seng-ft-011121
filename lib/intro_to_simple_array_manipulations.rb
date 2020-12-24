13b450d8822e2b1f27b66f9cfba1ae8f7bb9d96b
Harrf Akbar <harrf_akbar@yahoo.com>
2020-12-24 00:44:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,4 +3,11 @@ def using_concat(array1, array2)
   return array1
 end
 
-def using_insert()
+def using_insert(array, element)
+  array.insert(element)
+  return array
+end
+
+def using_uniq(array)
+  array.uniq 
+end
